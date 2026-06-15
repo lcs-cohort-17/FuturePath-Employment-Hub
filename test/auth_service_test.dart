f86a1b0fcb91d54700ca8986f90d2117ec8cf3b5
Charlton <charltonpoole46@gmail.com>
2026-06-15 15:58:01 +0200

    AUTH-002 Add test files

--- test/auth_service_test.dart
+++ test/auth_service_test.dart
@@ -1,1 +1,28 @@
-// gvjy
+import 'package:flutter_test/flutter_test.dart';
+import 'package:futurepath_employment_hub/services/auth_services.dart';
+
+void main() {
+  group('AuthService Tests', () {
+    test('AuthService should be a singleton', () {
+      final auth1 = AuthService();
+      final auth2 = AuthService();
+
+      expect(identical(auth1, auth2), true);
+    });
+
+    test('AuthService instance should not be null', () {
+      final auth = AuthService();
+
+      expect(auth, isNotNull);
+    });
+
+    test('AuthService exposes required methods', () {
+      final auth = AuthService();
+
+      expect(auth.signIn, isNotNull);
+      expect(auth.signUp, isNotNull);
+      expect(auth.signOut, isNotNull);
+      expect(auth.resetPassword, isNotNull);
+    });
+  });
+}
--- test/home_screen_test.dart
+++ test/home_screen_test.dart
@@ -1,1 +1,16 @@
-// hcjyg
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:futurepath_employment_hub/screens/home/home_screen.dart';
+
+void main() {
+  testWidgets('HomeScreen loads', (WidgetTester tester) async {
+    await tester.pumpWidget(
+      const MaterialApp(
+        home: HomeScreen(),
+      ),
+    );
+
+    expect(find.byType(HomeScreen), findsOneWidget);
+  });
+}
