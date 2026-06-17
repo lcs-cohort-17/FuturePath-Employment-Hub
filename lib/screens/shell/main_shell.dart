ab216fea7abfddf8f4c682e2dc3de0d17332dc03
Abdul-Jaleel Mangala <topsoldier1aj@gmail.com>
2026-06-17 15:45:40 +0200

    Sync latest auth + core updates from sibahle branch

--- lib/screens/shell/main_shell.dart
+++ lib/screens/shell/main_shell.dart
@@ -1,1 +1,17 @@
-// hgftyj
+import 'package:flutter/material.dart';
+
+class AppShell extends StatelessWidget {
+  const AppShell({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('FuturePath'),
+      ),
+      body: const Center(
+        child: Text('Home Screen - Coming Soon'),
+      ),
+    );
+  }
+}
