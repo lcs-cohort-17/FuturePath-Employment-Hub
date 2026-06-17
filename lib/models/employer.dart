a63da9e227a9ab5ca4d29cb8c35d7ac5ad37b6db
unknown <marcofisher21@gmail.comgit config --global user.email marcofisher21@gmail.comgit config --global user.name Marcogit config --global user.email marcofisher21@gmail.com>
2026-06-17 14:45:43 +0200

    added sprint 1's tickets

--- lib/models/employer.dart
+++ lib/models/employer.dart
@@ -1,1 +1,34 @@
-// gfiy
+class EmployerModel {
+  final String id;
+  final String companyName;
+  final String industry;
+  final String location;
+  final String? website;
+  final String? email;
+  final String? bio;
+  final int activeOpeningsCount;
+
+  EmployerModel({
+    required this.id,
+    required this.companyName,
+    required this.industry,
+    required this.location,
+    this.website,
+    this.email,
+    this.bio,
+    required this.activeOpeningsCount,
+  });
+
+  factory EmployerModel.fromJson(Map<String, dynamic> json) {
+    return EmployerModel(
+      id: json['id'] ?? '',
+      companyName: json['company_name'] ?? '',
+      industry: json['industry'] ?? '',
+      location: json['location'] ?? '',
+      website: json['website'],
+      email: json['email'],
+      bio: json['bio'],
+      activeOpeningsCount: json['active_openings_count'] ?? 0,
+    );
+  }
+}
