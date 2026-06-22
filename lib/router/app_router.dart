293c768748ddd0d8f7af107173c3295b7400953c
damonliftz <aidendamon16@gmail.com>
2026-06-22 14:58:29 +0200

    damon's work

--- lib/router/app_router.dart
+++ lib/router/app_router.dart
@@ -2,11 +2,15 @@ import 'package:flutter/material.dart';
 import '../screens/auth/login_screen.dart';
 import '../screens/auth/forgot_password_screen.dart';
 import '../screens/shell/main_shell.dart';
+import '../screens/jobs/opportunity_list_screen.dart';
+import '../screens/jobs/opportunity_detail_screen.dart';
 
 class AppRouter {
   static const String login = '/login';
   static const String forgotPassword = '/forgot-password';
   static const String home = '/home';
+  static const String jobs = '/jobs';
+  static const String jobDetail = '/job-detail';
 
   static Route<dynamic> generateRoute(RouteSettings settings) {
     switch (settings.name) {
@@ -23,6 +27,17 @@ class AppRouter {
       case home:
         return MaterialPageRoute(
           builder: (_) => const AppShell(),
+          settings: settings,
+        );
+      case jobs:
+        return MaterialPageRoute(
+          builder: (_) => const OpportunityListScreen(),
+          settings: settings,
+        );
+      case jobDetail:
+        final opportunity = settings.arguments as Opportunity;
+        return MaterialPageRoute(
+          builder: (_) => OpportunityDetailScreen(opportunity: opportunity),
           settings: settings,
         );
       default:
