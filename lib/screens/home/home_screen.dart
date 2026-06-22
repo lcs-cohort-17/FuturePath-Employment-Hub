5c6b87d1a37d087c1363050c7cc6a78aa7ba0446
Khangelani Mhlakane <kmhlakane@gmail.com>
2026-06-22 15:22:07 +0200

    ive added my tab nav

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,1 +1,28 @@
-// hgyj
+import 'package:flutter/material.dart';
+import 'search_results_screen.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          child: const Text('Open Search Results'),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (_) => const SearchResultsScreen(),
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
