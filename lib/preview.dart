e494b374b2bb6519a2ae9be0c05fcba99ad9c9e9
Phoenix <jojoeydenver@gmail.com>
2026-06-25 12:09:00 +0200

    uiux-018 mock

--- lib/preview.dart
+++ lib/preview.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import 'core/theme/app_theme.dart';
-import 'screens/jobs/job_list_screen.dart'; // Import the screen
+import 'screens/admin/admin_performance_screen.dart'; // Import the screen
 
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     theme: AppTheme.lightTheme,
-    home: const OpportunityListScreen(), // Set it as the home screen
+    home: const AdminPerformanceScreen(), // Set it as the home screen
   ));
 }
